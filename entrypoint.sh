b7451b9afa1588435ad268dcdb6deef7cb614913
Stuart Norton <stuart.norton@datamigrators.com>
2026-03-31 15:45:50 +1100

    Exception changes and cli log checks for all

--- entrypoint.sh
+++ entrypoint.sh
@@ -140,6 +140,8 @@ EOF
     echo '</details>'
   } >>"$GITHUB_STEP_SUMMARY"
 
+  find / -name "cli.*.log" -ls
+
   if [[ -f "${MCIX_LOG_DIR}/cli.$(date +%F).log" ]]; then
     {
       # Display the contents of the mcix command's log file. (collapsed by default)
@@ -151,21 +153,23 @@ EOF
       echo '```'
       echo '</details>'
     } >>"$GITHUB_STEP_SUMMARY"
+  else
+      gh_warn "Log file for '${MCIX_CMD_NAME}' not found" "Continuing without failing the action."
   fi
 
-  if [[ -f "${MCIX_LOG_DIR}/exception.$(date +%F).log" ]]; then
+  for file in $MCIX_LOG_DIR/exception.*.log; do
+    echo " - $file"
     {
       # Display the contents of the mcix command's log file. (collapsed by default)
       echo '<details>'
-      echo "<summary>Exception Log - ${MCIX_LOG_DIR}/exception.$(date +%F).log</summary>"
+      echo "<summary>Exception Log - $file</summary>"
       echo # A blank line after the <summary> tag is required by GitHub to format the content correctly
       echo '```'
-      cat "${MCIX_LOG_DIR}/exception.$(date +%F).log"
+      cat $file
       echo '```'
       echo '</details>'
     } >>"$GITHUB_STEP_SUMMARY"
-  fi
-
+  done
 }
 
 # ---------
